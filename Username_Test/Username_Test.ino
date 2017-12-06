50ee1c78dc2c1ca21753bf08063a1ca06fb3e97a
Mitchell Hwang <mdhwang@mit.edu>
2017-12-05 21:03:03 -0500

    added spiffs code

--- Username_Test/Username_Test.ino
+++ Username_Test/Username_Test.ino
@@ -9,6 +9,9 @@ ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti cl
 
 ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
 
+String getContentType(String filename);
+bool handleFileRead(String path);
+
 void handleRoot();              // function prototypes for HTTP handlers
 void handleLogin();
 void handleNotFound();
@@ -40,11 +43,17 @@ void setup(void){
     Serial.println("Error setting up MDNS responder!");
   }
 
+  SPIFFS.begin();
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "404: Not Found");
+  });
+  
   // Display the home page
-  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/" 
-  // When the user logs in successfully, go to /login
-  server.on("/login", HTTP_POST, handleLogin); // Call the 'handleLogin' function when a POST request is made to URI "/login"
-  server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+//  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/" 
+//  // When the user logs in successfully, go to /login
+//  server.on("/login", HTTP_POST, handleLogin); // Call the 'handleLogin' function when a POST request is made to URI "/login"
+//  server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
 
   server.begin();                            // Actually start the server
   Serial.println("HTTP server started");
@@ -52,6 +61,33 @@ void setup(void){
 
 void loop(void){
   server.handleClient();                     // Listen for HTTP requests from clients
+}
+
+String getContentType(String filename) { // convert the file extension to the MIME type
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path){  // send the right file to the client (if it exists)
+  Serial.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.html";           // If a folder is requested, send the index file
+  String contentType = getContentType(path);             // Get the MIME type
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){  // If the file exists, either as a compressed archive, or normal
+    if(SPIFFS.exists(pathWithGz))                          // If there's a compressed version available
+      path += ".gz";                                         // Use the compressed version
+    File file = SPIFFS.open(path, "r");                    // Open the file
+    size_t sent = server.streamFile(file, contentType);    // Send it to the client
+    file.close();                                          // Close the file again
+    Serial.println(String("\tSent file: ") + path);
+    return true;
+  }
+  Serial.println(String("\tFile Not Found: ") + path);
+  return false;                                          // If the file doesn't exist, return false
 }
 
 void handleRoot() {                          // When URI / is requested, send a web page with a button to toggle the LED

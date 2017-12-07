636272c55e837aff4f0e3775132c6d49a981dfdf
Mitchell Hwang <mdhwang@mit.edu>
2017-12-07 01:23:01 -0500

    Redirects working, halfway finished

--- Username_Test/Username_Test.ino
+++ Username_Test/Username_Test.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WiFiMulti.h> 
+#include <ESP8266WiFiMulti.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
 #include <FS.h>
@@ -12,18 +12,29 @@ ESP8266WebServer server(80);    // Create a webserver object that listens for HT
 String getContentType(String filename);
 bool handleFileRead(String path);
 
+String user[2];
+int maxScore;
+
 void handleRoot();              // function prototypes for HTTP handlers
 void handleLogin();
+void handleLeaderboard();
+void handleHistory();
+void handleScream();
+void handleScreaming();
+void handlePost();
+void handleLogout();
 void handleNotFound();
 
-void setup(void){
+void setup(void) {
+  user[0] = "";
+  user[1] = "";
   Serial.begin(115200);         // Start the Serial communication to send messages to the computer
   delay(10);
   Serial.println('\n');
 
   wifiMulti.addAP("MIT", "");   // add Wi-Fi networks you want to connect to
-//  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
-//  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+  //  wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
+  //  wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
 
   Serial.println("Connecting ...");
   int i = 0;
@@ -44,86 +55,119 @@ void setup(void){
   }
 
   SPIFFS.begin();
-//  server.onNotFound([]() {
-//    if (!handleFileRead(server.uri()))
-//      server.send(404, "text/plain", "404: Not Found");
-//  });
-  
+  //  server.onNotFound([]() {
+  //    if (!handleFileRead(server.uri()))
+  //      server.send(404, "text/plain", "404: Not Found");
+  //  });
+
   // Display the home page
-  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/" 
-//  // When the user logs in successfully, go to /login
+  server.on("/", HTTP_GET, handleRoot);        // Call the 'handleRoot' function when a client requests URI "/"
+  //  // When the user logs in successfully, go to /login
   server.on("/login", HTTP_POST, handleLogin); // Call the 'handleLogin' function when a POST request is made to URI "/login"
+  server.on("/leaderboard", HTTP_POST, handleLeaderboard);
+  server.on("/history", HTTP_POST, handleHistory);
+  server.on("/scream", HTTP_POST, handleScream);
+  server.on("/screaming", HTTP_POST, handleScreaming);
+  server.on("/post", HTTP_POST, handlePost);
+  server.on("/", HTTP_POST, handleLogout);
   server.onNotFound(handleNotFound);           // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
 
   server.begin();                            // Actually start the server
   Serial.println("HTTP server started");
 }
 
-void loop(void){
+void loop(void) {
   server.handleClient();                     // Listen for HTTP requests from clients
 }
 
-String getContentType(String filename) { // convert the file extension to the MIME type
-  if (filename.endsWith(".html")) return "text/html";
-  else if (filename.endsWith(".css")) return "text/css";
-  else if (filename.endsWith(".js")) return "application/javascript";
-  else if (filename.endsWith(".ico")) return "image/x-icon";
-  else if(filename.endsWith(".gz")) return "application/x-gzip";
-  return "text/plain";
+//String getContentType(String filename) { // convert the file extension to the MIME type
+//  if (filename.endsWith(".html")) return "text/html";
+//  else if (filename.endsWith(".css")) return "text/css";
+//  else if (filename.endsWith(".js")) return "application/javascript";
+//  else if (filename.endsWith(".ico")) return "image/x-icon";
+//  else if (filename.endsWith(".gz")) return "application/x-gzip";
+//  return "text/plain";
+//}
+
+//bool handleFileRead(String path) { // send the right file to the client (if it exists)
+//  Serial.println("handleFileRead: " + path);
+//  if (path.endsWith("/")) path += "index.html";          // If a folder is requested, send the index file
+//  String contentType = getContentType(path);             // Get the MIME type
+//  String pathWithGz = path + ".gz";
+//  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) { // If the file exists, either as a compressed archive, or normal
+//    if (SPIFFS.exists(pathWithGz))                         // If there's a compressed version available
+//      path += ".gz";                                         // Use the compressed version
+//    File file = SPIFFS.open(path, "r");                    // Open the file
+//    size_t sent = server.streamFile(file, contentType);    // Send it to the client
+//    file.close();                                          // Close the file again
+//    Serial.println(String("\tSent file: ") + path);
+//    return true;
+//  }
+//  Serial.println(String("\tFile Not Found: ") + path);
+//  return false;                                          // If the file doesn't exist, return false
+//}
+
+void handleRoot() {                          // When URI / is requested, send the home page
+  server.send(200, "text/html", "<p>Welcome to 6.S06Scream!!!</p><form action=\"/login\" method=\"POST\">Firstname: <input type=\"text\" name=\"firstname\" placeholder=\"John\"></br>Lastname: <input type=\"text\" name=\"lastname\" placeholder=\"Doe\"></br><input type=\"submit\" value=\"Login\"></form><form action=\"/leaderboard.html\" method=\"POST\"><input type=\"submit\" value=\"Leaderboard\"></form>");
+
+//  File readLog = SPIFFS.open("/temp.csv", "r");
+//  for (int i = 0; i < 10; i++){
+//    Serial.println(readLog.readStringUntil('\n'));
+//  }
 }
 
-bool handleFileRead(String path){  // send the right file to the client (if it exists)
-  Serial.println("handleFileRead: " + path);
-  if(path.endsWith("/")) path += "index.html";           // If a folder is requested, send the index file
-  String contentType = getContentType(path);             // Get the MIME type
-  String pathWithGz = path + ".gz";
-  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){  // If the file exists, either as a compressed archive, or normal
-    if(SPIFFS.exists(pathWithGz))                          // If there's a compressed version available
-      path += ".gz";                                         // Use the compressed version
-    File file = SPIFFS.open(path, "r");                    // Open the file
-    size_t sent = server.streamFile(file, contentType);    // Send it to the client
-    file.close();                                          // Close the file again
-    Serial.println(String("\tSent file: ") + path);
-    return true;
-  }
-  Serial.println(String("\tFile Not Found: ") + path);
-  return false;                                          // If the file doesn't exist, return false
-}
-
-void handleRoot() {                          // When URI / is requested, send a web page with a button to toggle the LED
-  server.send(200, "text/html", "<p>What is your name?</p><form action=\"/login\" method=\"POST\">Firstname: <input type=\"text\" name=\"firstname\" placeholder=\"John\"></br>Lastname: <input type=\"text\" name=\"lastname\" placeholder=\"Doe\"></br><input type=\"submit\" value=\"Submit\"></form>");
-  
-  File tempLog = SPIFFS.open("/temp.csv", "a"); // Write the time and the temperature to the csv file
-      tempLog.print(300);
-      tempLog.print(',');
-      tempLog.println(400);
-      tempLog.close();
-
-  File readLog = SPIFFS.open("/temp.csv", "r");
-      Serial.print(readLog.readStringUntil('\n'));
-//  server.send(200, "text/html", "<form action=\"/login\" method=\"POST\"><input type=\"text\" name=\"username\" placeholder=\"Username\"></br><input type=\"password\" name=\"password\" placeholder=\"Password\"></br><input type=\"submit\" value=\"Login\"></form><p>Try 'John Doe' and 'password123' ...</p>");
-}
 void handleLogin() {                         // If a POST request is made to URI /login
-  if( ! server.hasArg("firstname") || ! server.hasArg("lastname")
-      || server.arg("firstname") == NULL || server.arg("lastname") == NULL){
+  if (user[0] != "" && user[1] != ""){
+    server.send(200, "text/html", "<p>Welcome, " + user[0] + " " + user[1] + "!</p><form action=\"/history\" method=\"POST\"><input type=\"submit\" value=\"My Scores\"></form><form action=\"/scream\" method=\"POST\"><input type=\"submit\" value=\"Scream\"></form><form action=\"/leaderboard\" method=\"GET\"><input type=\"submit\" value=\"Leaderboard\"></form><form action=\"/\" method=\"POST\"><input type=\"submit\" value=\"Logout\"></form>");
+  }
+  else if ( ! server.hasArg("firstname") || ! server.hasArg("lastname")
+       || server.arg("firstname") == NULL || server.arg("lastname") == NULL) {
     server.send(400, "text/plain", "400: Invalid Request, please enter your name");
     return;
   }
   else {
-    server.send(200, "text/html", "<h1>Welcome, " + server.arg("firstname") + " " + server.arg("lastname") + "!</h1><p>Would you like to play?</p>");
+    user[0] = server.arg("firstname");
+    user[1] = server.arg("lastname");
+//    SPIFFS.remove("/temp.csv");
+//    File tempLog = SPIFFS.open("/temp.csv", "a");
+//    tempLog.print(server.arg("firstname") + " " + server.arg("lastname") + ".\n");
+//    tempLog.close();
+
+    server.send(200, "text/html", "<p>Welcome, " + user[0] + " " + user[1] + "!</p><form action=\"/history\" method=\"POST\"><input type=\"submit\" value=\"My Scores\"></form><form action=\"/scream\" method=\"POST\"><input type=\"submit\" value=\"Scream\"></form><form action=\"/leaderboard\" method=\"GET\"><input type=\"submit\" value=\"Leaderboard\"></form><form action=\"/\" method=\"POST\"><input type=\"submit\" value=\"Logout\"></form>");
   }
-//  if( ! server.hasArg("username") || ! server.hasArg("password") 
-//      || server.arg("username") == NULL || server.arg("password") == NULL) { // If the POST request doesn't have username and password data
-//    server.send(400, "text/plain", "400: Invalid Request");         // The request is invalid, so send HTTP status 400
-//    return;
-//  }
-//  if(server.arg("username") == "John Doe" && server.arg("password") == "password123") { // If both the username and the password are correct
-//    server.send(200, "text/html", "<h1>Welcome, " + server.arg("username") + "!</h1><p>Login successful</p>");
-//  } else {                                                                              // Username and password don't match
-//    server.send(401, "text/plain", "401: Unauthorized");
-//  }
 }
 
-void handleNotFound(){
+void handleLeaderboard(){
+  
+}
+
+void handleHistory(){
+  
+}
+
+void handleScream(){
+  if (maxScore != 0){ // If the user came back from /screaming, then there is a maxScore for the session
+    String maxScoreString = String(maxScore);
+    server.send(200, "text/html", "<p>Scream volume: " + maxScoreString + "</p><form action=\"/post\" method=\"POST\"><input type=\"submit\" value=\"Post\"></form>");
+  } else{
+    server.send(200, "text/html", "<p>Scream volume: </p><form action=\"/screaming\" method=\"POST\"><input type=\"submit\" value=\"Start\"></form><form action=\"/post\" method=\"POST\"><input type=\"submit\" value=\"Post\"></form><form action=\"/login\" method=\"POST\"><input type=\"submit\" value=\"Home\"></form>");
+  }
+}
+
+void handleScreaming(){
+  server.send(200, "text/html", "<form action=\"/scream\" method=\"POST\"><input type=\"submit\" value=\"Stop\"></form>");
+}
+
+void handlePost(){
+  
+}
+
+void handleLogout(){
+  user[0] = "";
+  user[1] = "";
+  handleRoot();
+}
+
+void handleNotFound() {
   server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
